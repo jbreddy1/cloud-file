038d24aafa12cfa4f5d67c975bf381aaa8273982
jbreddy1 <jbreddyrajula@gmail.com>
2025-09-11 18:41:43 +0530

    fixed secure code

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 // Firebase configuration
-var firebaseConfig = {
-  apiKey: "AIzaSyDqtrSFObjzIC-MZ_fGbBK0iVwXpH-hKX8",
-  authDomain: "dad-upload.firebaseapp.com",
-  projectId: "dad-upload",
-  storageBucket: "dad-upload.firebasestorage.app",
-  messagingSenderId: "617576089214",
-  appId: "1:617576089214:web:dd6284ba410fe865cbfa2f",
-  measurementId: "G-Z3XVKP235H"
-};
+
+
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
